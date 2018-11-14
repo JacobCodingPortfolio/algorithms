80510b7423d1e8f00c0e239362261aba6ad1ef02
JNartowicz <nartowiczjakub@gmail.com>
2018-11-14 13:41:29 +0100

    I have done some work with binomial algorithm.

--- jcp-binomial-theorem/src/main/java/pl/com/bt/BinomialTheoremAlgorithm.java
+++ jcp-binomial-theorem/src/main/java/pl/com/bt/BinomialTheoremAlgorithm.java
@@ -1,4 +1,18 @@
 package pl.com.bt;
 
+import java.util.List;
+
 public class BinomialTheoremAlgorithm {
+
+    public static List<Long> binomialTheorem(List<Long[]> numbersToResolve){
+
+        return null;
+    }
+
+    private static Long binomialTheoremRecursively(Long a, Long b){
+
+        return null;
+    }
+
+
 }
--- jcp-binomial-theorem/src/main/java/pl/com/bt/UserInterface.java
+++ jcp-binomial-theorem/src/main/java/pl/com/bt/UserInterface.java
@@ -1,13 +1,34 @@
 package pl.com.bt;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class UserInterface extends Application {
 
+    private static final String FXML_FILE_NAME_PATH = "/UserInterface.fxml";
+    private static UserInterface userInterface;
+    private UserInterfaceController interfaceController;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
-
+        userInterface = this;
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(FXML_FILE_NAME_PATH));
+        Parent parent = fxmlLoader.load();
+        this.interfaceController = fxmlLoader.getController();
+        Scene scene = new Scene(parent);
+        primaryStage.setScene(scene);
+        primaryStage.sizeToScene();
+        primaryStage.show();
     }
 
+    public static synchronized UserInterface getUserInterface() {
+        return userInterface;
+    }
+
+    public UserInterfaceController getInterfaceController() {
+        return interfaceController;
+    }
 }
