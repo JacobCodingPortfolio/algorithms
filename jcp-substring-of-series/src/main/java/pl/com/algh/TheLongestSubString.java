2b995d89a7bea8d691371654b92d87abeb0f0610
JNartowicz <nartowiczjakub@gmail.com>
2018-11-23 20:34:57 +0100

    ss

--- jcp-substring-of-series/src/main/java/pl/com/algh/TheLongestSubString.java
+++ jcp-substring-of-series/src/main/java/pl/com/algh/TheLongestSubString.java
@@ -6,9 +6,25 @@ public class TheLongestSubString {
 
     }
 
-    private String getTheLongestSubString(){
-
+    private String getTheLongestSubString(String s1, String s2){
+        //Tworzenie tablicy dwu wymiarowej
+        int m = s1.length() + 1;
+        int n = s2.length() + 1;
         
+        int[][] s = new int[m][n];
+
+        //Wypełnienie tablic zerami
+        for(int i=0; i< m; i++) s[i][0] = 0;
+        for(int i=0; i< n; i++) s[0][i] = 0;
+
+        for (int i=1; i<m; i++){
+            for (int j=1; j<n; j++){
+
+
+
+            }
+        }
+
 
         return null;
     }
